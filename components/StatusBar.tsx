e6a17af7cd8ba5351625c792210af457c1ec6001
alemes7 <alemesferreira7@gmail.com>
2024-04-25 21:07:29 -0300

    update

--- components/StatusBar.tsx
+++ components/StatusBar.tsx
@@ -3,36 +3,33 @@ import { View, Image, Text, StyleSheet } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 
 type Props = {
-    title: string;
+  title: string;
 };
 
-export default function Status({ title }: Props) {
-    const styles = StyleSheet.create({
-        container: {
-            alignItems: 'center',
-            justifyContent: 'center',
-            marginTop: 40,
-            height: 200,
-        },
-        image: {
-            resizeMode: 'contain',
-            width: 200,
-            height: 100,
-        },
-        texto: {
-            color: 'white',
-            fontSize: 20,
-            
-        },
-    });
-
-    return (
-        <View style={styles.container}>
-            <Image source={require('@assets/logo-senai.png')} style={styles.image} />
-            <StatusBar style="light" backgroundColor="#333" />
-            <Text style={styles.texto}>{title}</Text>
-        </View>
-    );
+export default function Status ({ title }: Props){
+  const styles = StyleSheet.create({
+    container: {
+      alignItems: 'center',
+      justifyContent: 'center',
+      padding: 60,
+    },
+    image: {
+      resizeMode: 'contain',
+    },
+    texto: {
+      color: 'white',
+      fontSize: 20,
+      padding: 5,
+    },
+  });
+  
+  return (
+    <View style={styles.container}>
+      <Image source={require('@assets/Logo.png')} style={styles.image} />
+      <StatusBar style="light" backgroundColor="#333" />
+      <Text style={styles.texto}>{title}</Text>
+    </View>
+  );
 };
 
 
