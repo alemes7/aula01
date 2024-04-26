1ba58af0301feb13670aa5cb952c842c2586f192
alemes7 <alemesferreira7@gmail.com>
2024-04-25 22:16:12 -0300

    update final, com alterações no additens, descricao e etc

--- app/TabNav/DrawerNav/Home/_layout.tsx
+++ app/TabNav/DrawerNav/Home/_layout.tsx
@@ -1,6 +1,9 @@
 import { DrawerToggleButton } from "@react-navigation/drawer";
-import { Stack } from "expo-router";
+import { Link, Stack } from "expo-router";
 import { useColor } from "../../../../temas/temas";
+import React from "react";
+import { Ionicons } from "@expo/vector-icons";
+import { TouchableOpacity } from "react-native";
 
 export default function layout() {
     const cores = useColor();
@@ -9,13 +12,55 @@ export default function layout() {
             <Stack.Screen
                 name="Lista"
                 options={{
-                    headerLeft: () => <DrawerToggleButton />,
-                    headerSearchBarOptions: { placeholder: "Pesquisar" },
-                    headerStyle: { backgroundColor: cores.bgPatrimonio },
+                    headerLeft: () => <DrawerToggleButton tintColor= {cores.textColorPrimaryVariant} />,
+                    headerSearchBarOptions: { placeholder: "Pesquisar", headerIconColor: cores.textColorPrimaryVariant},
+                    headerStyle: { backgroundColor: cores.bglistaPrimary },
                     headerTitleAlign: 'center',
-                    headerTintColor: cores.textColorPrimary,
+                    headerTintColor: cores.textColorPrimaryVariant,
+                    
                 }}
             />
+
+            {/* Esta e a Tela de Descrição */}
+            <Stack.Screen name="DescricaoIten" options={{
+                headerRight: () => (
+                <TouchableOpacity>
+                        <Ionicons name="reload" size={24} color='white' />
+                </TouchableOpacity>
+                ),
+                headerTitle: "Descrição de item",
+                headerTintColor: "#fff",
+                headerStyle: { backgroundColor: cores.bgPatrimonio },
+                headerTitleAlign: 'center'
+            }}/>
+            {/* Esta e a Tela de Lista */}
+            <Stack.Screen name="Additens" options={{
+                headerRight: () => (
+                <TouchableOpacity>
+                    <Link href="/TabNav/DrawerNav/Home/Editar" asChild>
+                        <Ionicons name="add-circle-outline" size={24} color='white' />
+                    </Link>
+                </TouchableOpacity>
+                ),
+                headerTitle: "Cadastro de itens",
+                headerTintColor: "#fff",
+                headerStyle: { backgroundColor: cores.bgPatrimonio },
+                headerTitleAlign: 'center'
+            }}/>
+
+            <Stack.Screen name="Editar" options={{
+                headerRight: () => (
+                <TouchableOpacity>
+                    <Link href="/TabNav/DrawerNav/Home/Lista" asChild>
+                        <Ionicons name="checkmark-circle" size={24} color='white' />
+                    </Link>
+                </TouchableOpacity>
+                ),
+                headerTitle: "Cadastro de itens",
+                headerTintColor: "#fff",
+                headerStyle: { backgroundColor: cores.bgPatrimonio },
+                headerTitleAlign: 'center'
+            }}/>
         </Stack>
     )
 }
